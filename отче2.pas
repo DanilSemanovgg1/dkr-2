8b3e1778cd60bd37b091c62dc5adce852e0d278a
DanilSemanovgg1 <144227090+DanilSemanovgg1@users.noreply.github.com>
2023-11-28 16:41:15 +0300

    Add files via upload

---
+++ отче2.pas
@@ -0,0 +1,29 @@
+﻿  var
+  str1, str2: string;
+  a, b: integer;
+
+begin
+  write('Введите исходную строку: ');
+  readln(str1);
+  
+  write('Введите строку ввода: ');
+  readln(str2);
+  
+  a := 1;
+  b := 1;
+  
+  for a := 1 to length(str1) do
+  begin
+    if str1[a] = str2[b] then
+    begin
+      b := b + 1;
+      if b > length(str2) then
+        break;
+    end;
+  end;
+  
+  if b > length(str2) then
+    writeln('Входная строка является подпоследовательностью исходной строки')
+  else
+    writeln('Входная строка не является подпоследовательностью исходной строки');
+end.
---
+++ отчет.pas
@@ -0,0 +1,23 @@
+﻿var
+  mas: array of Integer;
+  n, i, k: Integer;
+
+begin
+  
+  write('Введите размер массива: ');
+  readln(n);
+  SetLength(mas, n);
+  writeln('Введите элементы массива: ');
+  for i := 0 to n - 1 do
+    readln(mas[i]);
+  
+  for i := 0 to n - 2 do
+  begin
+    if mas[i + 1] > mas[i] then
+    else
+      k:= 1;   
+  end;
+  if k = 1 then 
+    writeln('массив не возрастающий ')
+  else writeln('Массив возрастающий');
+end.
